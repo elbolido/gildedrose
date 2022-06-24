c31773ec03af3550dfc38994ec2ef6d0238fd258
Simon Masliah <simon.masliah@octo.com>
2022-06-24 16:21:29 +0200

    first commit

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+    Item[] itemList = new Item[]{
+        new Item("+5 Dexterity Vest", 10, 20), //
+        new Item("Aged Brie", 2, 0), //
+        new Item("Elixir of the Mongoose", 5, 7), //
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+        new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        // this conjured item does not work properly yet
+        new Item("Conjured Mana Cake", 3, 6)};
+
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = itemList;
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
 
+        Item[] expected = new Item[]{
+            new Item("+5 Dexterity Vest", 9, 19), //
+            new Item("Aged Brie", 1, 1), //
+            new Item("Elixir of the Mongoose", 4, 6), //
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 14, 21),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 9, 50),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 4, 50),
+            // this conjured item does not work properly yet
+            new Item("Conjured Mana Cake", 2, 5)};
+
+        String[] itemNames = new String[] {
+
+        }
+
+        app.updateQuality();
+        assertEquals(expected, items);
+    }
 }
